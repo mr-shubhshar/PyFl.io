a05270ae8303ac66bf80335d979f6cfd6520b4ce
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-12 02:07:25 +0530

    defined db models

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' %self.name
