b77cf74cdd57c208f3ce6fe6a0b07a1d81110d09
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-12 02:18:53 +0530

    fixed: added User db model

--- app/models.py
+++ app/models.py
@@ -10,3 +10,9 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role %r>' %self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
