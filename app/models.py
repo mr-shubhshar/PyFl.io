ddea65d7344ef33bd1824f74d3ceacbf45a37c8e
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-12 02:21:01 +0530

    added password hashing

--- app/models.py
+++ app/models.py
@@ -16,3 +16,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+## following are attributes for password hashing ##
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
