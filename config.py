767733e6c6a6be60e579fc8610fb84f5131dbcf9
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-23 22:13:29 +0530

    fixes

--- config.py
+++ config.py
@@ -21,11 +21,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
